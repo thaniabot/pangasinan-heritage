cba49d106c4accb67f9c91dd308813d990183f7b
Elmer Roces <roceselemr901@gmail.com>
2026-09-07 07:35:05 +0800

    Fix basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-
+  basePath: '/pangasinan-heritage',
   images: {
     unoptimized: true,
   },
